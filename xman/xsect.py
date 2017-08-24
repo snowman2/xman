f8729051c9a2511920f6fcbdd04bbd60aed8b641
Alan Snow <alan.d.snow@erdc.dren.mil>
2017-08-24 16:21:49 -0500

    added docstring

--- xman/xsect.py
+++ xman/xsect.py
@@ -20,7 +20,54 @@ def x_section_from_latlon(elevation_file,
                           as_polygon=False,
                           auto_clean=False):
 
+    """
+    This workflow extracts a cross section from a DEM
+    based on the input latitude and longitude point pairs.
 
+    Parameters:
+    -----------
+    elevation_file: str
+        Path to the elevation DEM.
+    x_section_lat0: float
+        THe first coordinate latitude.
+    x_section_lon0: float
+        THe first coordinate longitude.
+    x_section_lat1: float
+        THe second coordinate latitude.
+    x_section_lon1: float
+        THe second coordinate longitude.
+    as_polygon: bool, optional
+        If True, will return cross section as a
+        :obj:`shapely.geometry.Polygon`. Default is False.
+    auto_clean: bool, optional
+        If True, will attempt to clean any issues from the polygon.
+        Default is False.
+
+    Returns:
+    --------
+    list or :obj:`shapely.geometry.Polygon`
+        Cross section information.
+        The list will be xy coordinate pairs.
+
+
+    Example::
+
+        from shapely.geometry import Polygon
+        from xman.xsect import x_section_from_latlon
+
+
+        elevation_file = '/path/to/elevation.tif'
+        lat1 = 34.105265417341442
+        lon1 = 38.993958690587505
+        lat2 = 34.107264451129197
+        lon2 = 38.99355588515526)
+        x_sect_list = x_section_from_latlon(elevation_file,
+                                            lat1,
+                                            lon1,
+                                            lat2,
+                                            lon2)
+
+    """
     utm_proj = utm_proj_from_latlon(x_section_lat0, x_section_lon0,
                                     as_osr=True)
     sp_ref = osr.SpatialReference()
